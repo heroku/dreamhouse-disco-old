018dc72d079a9ad8c3a14286ae5dc510ffacc5b6
Chris Castle <castle@heroku.com>
2016-10-01 22:27:59 -0700

    Find or create playlist

--- bin/controllers/playlist.js
+++ bin/controllers/playlist.js
@@ -7,6 +7,7 @@ let _ = require('lodash')
 let request = require('request')
 let getValidToken = require('../lib/token')
 var fmt = require('logfmt')
+let config = require('../config')
 
 class PlaylistController {
 
@@ -25,32 +26,75 @@ class PlaylistController {
   _getPlaylist(req, res) {
 
     this.db.Account.findOne({ })
-      .then(function(acct) {
+    .then(function(acct) {
 
-        getValidToken(acct)
-        .then(function(token) {
+      getValidToken(acct)
+      .then(function(token) {
 
-          const opts = { auth: { 'bearer': token } }
-          const playlistUrl = 'https://api.spotify.com/v1/users/' + acct.get('id') + '/playlists/' + acct.get('playlist_id')
+        // find or create playlist
+        // First, get list of user's playlists
+        const opts = { auth: { 'bearer': token }, json: true }
+        const playlistsUrl = `https://api.spotify.com/v1/users/${acct.get('id')}/playlists`
+        request.get(playlistsUrl, opts, function(error, response, body) {
+          if (error || response.statusCode != 200) {
+            fmt.log({
+              type: 'error',
+              msg: `Error: ${JSON.stringify(error || body)}`
+            })
+            return
+          }
 
-          request.get(playlistUrl, opts, function(error, response, body) {
-            const playlist = JSON.parse(body)
-            if (error || response.statusCode != 200) {
-              fmt.log({
-                type: 'warning',
-                msg: `Error: ${JSON.stringify(error || playlist)}`
+          let playlistFound = false;
+          let idx = 0;
+          while (!playlistFound && idx < body.items.length) {
+            if (body.items[idx].name === config.spotify.playlistName) {
+              // Found a playlist with a matching name
+              playlistFound = true
+              const playlistTracksHref = body.items[idx].tracks.href
+              request.get(playlistTracksHref, opts, function(error, response, body) {
+                if (error || (response.statusCode < 200 && response.statusCode >= 300)) {
+                  const msg = `Error getting playlist tracks: ${JSON.stringify(error || body)}`
+                  fmt.log({
+                    type: 'error',
+                    msg: msg
+                  })
+                  res.status(500).json({ error: msg })
+
+                } else {
+
+                  res.json({ tracks: body })
+                }
               })
-
-              // return faked empty playlist
-              res.json( { tracks: { items: [] } } )
-              return
             }
-            // TODO: better error handling
+            idx++
+          }
 
-            res.json(playlist)
-          })
+          if (!playlistFound) {
+            // Create a playlist and return it
+            const createOpts = Object.assign(opts, { body: { name: config.spotify.playlistName } })
+            request.post(playlistsUrl, createOpts, function(error, response, body) {
+              if (error || (response.statusCode < 200 && response.statusCode >= 300)) {
+                const msg = `Error creating playlist: ${JSON.stringify(error || body)}`
+                fmt.log({
+                  type: 'error',
+                  msg: msg
+                })
+                res.status(500).json({ error: msg })
+
+              } else {
+
+                fmt.log({
+                  type: 'info',
+                  msg: `No playlist found. Created one named '${config.spotify.playlistName}'`
+                })
+
+                res.json(body)
+              }
+            })
+          }
         })
       })
+    })
   }
 
 }
